759b54ddad61c1233a9885d1395efe513b822d00
linzj <manjian2006@gmail.com>
2016-03-04 21:26:53 +0800

    Reduce the amount of caculation.

--- gaussian-column.glsl
+++ gaussian-column.glsl
@@ -9,9 +9,9 @@ void main(void)
     int i;
     vec2 texcoord = (gl_FragCoord.xy + ivec2(0, c_blockSize / 2)) / vec2(u_screenGeometry);
     float toffset = 1.0 / u_screenGeometry.y;
-    vec4 color = texture2D(u_texture, texcoord) * vec4(u_kernel[0]);
+    vec3 color = texture2D(u_texture, texcoord).rgb * vec3(u_kernel[0]);
     for (i = 1; i < c_blockSize; ++i) {
-        color += texture2D(u_texture, texcoord - vec2(0.0, i * toffset)) * vec4(u_kernel[i]);
+        color += texture2D(u_texture, texcoord - vec2(0.0, i * toffset)).rgb * vec3(u_kernel[i]);
     }
-    gl_FragColor = vec4(color.rgb, 1.0);
+    gl_FragColor = vec4(color, 1.0);
 }
--- gaussian-row.glsl
+++ gaussian-row.glsl
@@ -9,9 +9,9 @@ void main(void)
     int i;
     vec2 texcoord = (gl_FragCoord.xy - ivec2(c_blockSize / 2, 0)) / vec2(u_screenGeometry);
     float toffset = 1.0 / u_screenGeometry.x;
-    vec4 color = texture2D(u_texture, texcoord) * vec4(u_kernel[0]);
+    vec3 color = texture2D(u_texture, texcoord).rgb * vec3(u_kernel[0]);
     for (i = 1; i < c_blockSize; ++i) {
-        color += texture2D(u_texture, texcoord + vec2(i * toffset, 0.0)) * vec4(u_kernel[i]);
+        color += texture2D(u_texture, texcoord + vec2(i * toffset, 0.0)).rgb * vec3(u_kernel[i]);
     }
-    gl_FragColor = vec4(color.rgb, 1.0);
+    gl_FragColor = vec4(color, 1.0);
 }
